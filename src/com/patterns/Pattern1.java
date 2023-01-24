fa28d100913834ae74e6035e71d604f9c5b723fe
neehhaa <nehawasnik95@outlook.com>
2023-01-24 22:06:31 +0530

    PATTERN 1

---
+++ src/com/patterns/Pattern1.java
@@ -0,0 +1,14 @@
+package com.patterns;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        int rows = 4;
+
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= rows; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
